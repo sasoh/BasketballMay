79406c5e390f41bd6fd683b6763c7d9bac270edb
Sasho <lxndrppv@gmail.com>
2015-05-06 00:24:22 +0300

    Camera now tracks general direction of the ball; jump pads particle lifetime set according to jump force

---
+++ Project/Basketball/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour
+{
+
+	public float maxHorizontal = 2.5f;
+	public float maxVertical = 10.0f;
+	private GameObject ballToFollow;
+
+	void Update()
+	{
+
+		UpdateBallPointer();
+		UpdateCameraRotation();
+
+	}
+
+	void UpdateBallPointer()
+	{
+
+		BallScript bScript = GameObject.FindObjectOfType<BallScript>();
+		ballToFollow = bScript.gameObject;
+
+	}
+
+	void UpdateCameraRotation()
+	{
+
+		// rotate cam around x
+		float angleX = (maxVertical / 2) - ballToFollow.transform.position.y;
+		Quaternion newRotationX = Quaternion.Euler(new Vector3(angleX, 0.0f, 0.0f));
+		transform.localRotation = Quaternion.Slerp(transform.localRotation, newRotationX, Time.deltaTime);
+		
+		// rotate cam around y
+		float angleY = (maxHorizontal / 2) - ballToFollow.transform.position.x;
+		Quaternion newRotationY = Quaternion.Euler(new Vector3(0.0f, -angleY, 0.0f));
+		transform.localRotation = Quaternion.Slerp(transform.localRotation, newRotationY, Time.deltaTime);
+
+	}
+
+}
--- Project/Basketball/Assets/Scripts/JumpPadScript.cs
+++ Project/Basketball/Assets/Scripts/JumpPadScript.cs
@@ -10,4 +10,23 @@ public class JumpPadScript : MonoBehaviour
 	[Tooltip("Timeout between consecutive pad jumps from same player. Needed to prevent double triggering when hitting the pad from strange angles.")]
 	public float jumpPadTimeout = 0.1f;
 
+	void Start()
+	{
+
+		SetupParticleSystemLifetime();	
+
+	}
+
+	void SetupParticleSystemLifetime()
+	{
+
+		ParticleSystem ps = gameObject.GetComponentInChildren<ParticleSystem>();
+		if (ps != null)
+		{
+			// set particle lifetime to something related to the jump pad force
+			ps.startLifetime *= jumpPadForce / 10.0f;
+		}
+
+	}
+
 }
